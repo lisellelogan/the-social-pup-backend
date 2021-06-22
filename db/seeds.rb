c36f9c166c5456ce17b2ae5f6e2746768f989695
lisellelogan <liselle.logan@yahoo.com>
2021-06-22 14:52:48 -0400

    create seed file to test associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Friendship.destroy_all
+
+beemo = User.create(email: 'beemo@beemo.com', password: 'beemo', pup_name: 'beemo', pup_age: 9, pup_breed: 'corgi', pup_fully_vaccinated: true, pup_location: 'queens, ny', pup_personality: 'crazy', owner_name: 'lis')
+seymour = User.create(email: 'seymour@seymour.com', password: 'seymour', pup_name: 'seymour', pup_age: 5, pup_breed: 'beagle', pup_fully_vaccinated: true, pup_location: 'virginia beach, va', pup_personality: 'calm', owner_name: 'london')
+charlie = User.create(email: 'charlie@charlie.com', password: 'charlie', pup_name: 'charlie', pup_age: 1, pup_breed: 'goldendoodle', pup_fully_vaccinated: true, pup_location: 'chesapeake, va', pup_personality: 'anxious', owner_name: 'justin')
+
+Friendship.create_mutual_friendship(beemo.id, seymour.id)
+Friendship.create_mutual_friendship(beemo.id, charlie.id)
+Friendship.create_mutual_friendship(seymour.id, charlie.id)
