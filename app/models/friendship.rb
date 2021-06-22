0926fd3b93a74f7e47b05bba9ac11594a3fd48c3
lisellelogan <liselle.logan@yahoo.com>
2021-06-22 14:43:39 -0400

    create method to create mutual friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,10 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  def self.create_mutual_friendship(user_id, friend_id)
+    Friendship.create(user_id: user_id, friend_id: friend_id)
+    Friendship.create(user_id: friend_id, friend_id: user_id)
+  end
+  
 end
