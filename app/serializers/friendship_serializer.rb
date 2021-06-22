0ddaece6ea68343cd013bee75f6a07c425208270
lisellelogan <liselle.logan@yahoo.com>
2021-06-22 15:39:45 -0400

    add attributes to Friendship serializer

--- app/serializers/friendship_serializer.rb
+++ app/serializers/friendship_serializer.rb
@@ -1,5 +1,6 @@
 class FriendshipSerializer
   include FastJsonapi::ObjectSerializer
   belongs_to :user
-  belongs_to :friend, class_name: "User" 
+  belongs_to :friend, class_name: "User"
+  attributes :user, :friend
 end
