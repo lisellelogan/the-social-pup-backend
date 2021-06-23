6f947632c6d4b6a7d4b04c894dff8ca49c78a735
lisellelogan <liselle.logan@yahoo.com>
2021-06-23 12:32:46 -0400

    include friendships in index action when rendering json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: UserSerializer.new(users)
+        render json: UserSerializer.new(users, {include: [:friendships]})
     end
 end
