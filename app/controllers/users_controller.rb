252c26ec78efb8a070c01f237205413969f2c498
lisellelogan <liselle.logan@yahoo.com>
2021-06-23 14:55:27 -0400

    remove curly braces as not needed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: UserSerializer.new(users, {include: [:friendships]})
+        render json: UserSerializer.new(users, include: [:friendships])
     end
 end
