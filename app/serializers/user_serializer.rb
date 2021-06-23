61dcc6e543f11db1c84e3e076760aa8173dec6aa
lisellelogan <liselle.logan@yahoo.com>
2021-06-23 14:26:58 -0400

    remove has many through relationship in serializer as not needed

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   has_many :friendships
-  has_many :friends, through: :friendships, source: :friend
-  attributes :email, :password, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :owner_name
+  # has_many :friends, through: :friendships, source: :friend
+  has_many :friends
+  attributes :email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :owner_name
 end
