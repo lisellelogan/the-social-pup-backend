65287093cc06e22f7a0c5b57a5dbd42d78030a7a
lisellelogan <liselle.logan@yahoo.com>
2021-06-23 14:32:28 -0400

    add custom attribute of friends

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,9 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   has_many :friendships
   # has_many :friends, through: :friendships, source: :friend
-  has_many :friends
-  attributes :email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :owner_name
+  def friends 
+    object.friends
+  end
+
+  attributes :email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :owner_name, :friends
 end
