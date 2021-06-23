4acbe1dca0b2a558adc59df82911066a02b2c822
lisellelogan <liselle.logan@yahoo.com>
2021-06-23 15:26:03 -0400

    add picture attribute to serializer to display to api

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -6,5 +6,5 @@ class UserSerializer
     object.friends
   end
 
-  attributes :email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :owner_name, :friends
+  attributes :email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :picture, :owner_name, :friends
 end
