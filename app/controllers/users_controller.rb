9d5ddf7a715beb598ee9d6ebb8f4d4633fbfd45f
lisellelogan <liselle.logan@yahoo.com>
2021-06-27 11:13:23 -0400

    add create action and strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,13 @@ class UsersController < ApplicationController
         users = User.all
         render json: UserSerializer.new(users, include: [:friendships])
     end
+
+    def create 
+        user = User.find_or_create_by(user_params)
+        render json: UserSerializer.new(user)
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :password, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :owner_name, :picture)
+    end
 end
