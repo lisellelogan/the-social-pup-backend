c2815dffb247116e87076147fe48955aa4f5f73a
lisellelogan <liselle.logan@yahoo.com>
2021-06-30 16:16:06 -0400

    create sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,15 @@
-class Sessions < ApplicationController
+class SessionsController < ApplicationController
+
+    def current_user
+        current_user = User.all.find_by(params[:email])
+    end
+
+    def logged_in?
+        if !!current_user
+            # render json success message
+            # return user show page
+            # else => display error message
+        end
+    end
 
 end
