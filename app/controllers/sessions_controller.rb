f8dbe763c4ff07b2a68c1a552c6f876cc188682f
lisellelogan <liselle.logan@yahoo.com>
2021-06-30 16:43:42 -0400

    fix current user and create custom route for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
 
     def current_user
-        current_user = User.all.find_by(params[:email])
-    end
-
-    def logged_in?
-        if !!current_user
+        current_user = User.find_by(email: params[:email])
+        if logged_in? === true
             # render json success message
             # return user show page
             # else => display error message
         end
+    end
+
+    def logged_in?
+        !!current_user
     end
 
 end
