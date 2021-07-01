7f32220685f156b78a82f0b5659521b2b2de2942
lisellelogan <liselle.logan@yahoo.com>
2021-07-01 10:49:08 -0400

    fix if statement for sessions#crate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
     def create
         current_user = User.find_by(email: params[:email])
-        if logged_in? === true
+        if current_user
             render json: {message: "login successful!"}
         else
             render json: {message: "We can't find you. Please try agian."}
