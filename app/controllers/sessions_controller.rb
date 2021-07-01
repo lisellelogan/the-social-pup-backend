daee85371234fd07e7c907109e4802beef2edd77
lisellelogan <liselle.logan@yahoo.com>
2021-07-01 11:32:33 -0400

    remove .to_json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         current_user = User.find_by(email: params[:email])
         if current_user
-            render json: current_user.to_json
+            render json: current_user
         else
             render json: {error: "We can't find you. Please try agian."}
         end
