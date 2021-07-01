6932496b35a3abf911c10d00097d62fba8b7c12d
lisellelogan <liselle.logan@yahoo.com>
2021-07-01 10:23:20 -0400

    change create action to only create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     def create 
-        user = User.find_by(user_params)
+        user = User.create(user_params)
         render json: UserSerializer.new(user)
     end
 
