d549cc9d5eea41877a7e3903ed22c91e53aaff29
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 17:17:01 -0400

    add destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
 
     def create
-        current_user = User.find_by(email: params[:email])
-        if current_user
-            render json: current_user
+        @current_user = User.find_by(email: params[:email])
+        if @current_user
+            render json: @current_user
         else
             render json: {error: "We can't find you. Please try agian."}
         end
+    end
+
+    def destroy
+        @current_user.clear
+        render json: {message: "logout successful. See you next time!"}
     end
 end
