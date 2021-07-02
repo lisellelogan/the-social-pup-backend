85e150466a3b550a005d9db2d86ea58b149cc4b5
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 19:04:49 -0400

    add create action for friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,4 +3,8 @@ class FriendshipsController < ApplicationController
         friendships = Friendship.all
         render json: FriendshipSerializer.new(friendships)
     end
+
+    def create 
+        friendship = Friendship.create_mutual_friendship(params[:user_id], params[:friend_id])
+    end
 end
