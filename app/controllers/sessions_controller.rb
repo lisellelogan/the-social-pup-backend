2e766a52e699f9b291a0f72dcd892ac0ec64f13c
lisellelogan <liselle.logan@yahoo.com>
2021-07-03 12:49:41 -0400

    have current user use UserSerializer to have access to friends attribute

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         @current_user = User.find_by(email: params[:email])
         if @current_user
-            render json: @current_user
+            render json: UserSerializer.new(@current_user)
         else
             render json: {error: "We can't find you. Please try agian."}
         end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -6,5 +6,5 @@ class UserSerializer
     object.friends
   end
 
-  attributes :email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :picture, :owner_name, :friends
+  attributes :id,:email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :picture, :owner_name, :friends
 end
