6ae0941491cb48fbf86ffc694010d781be698e98
lisellelogan <liselle.logan@yahoo.com>
2021-07-03 13:39:17 -0400

    make show for current user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
 
   post '/login', to: "sessions#create"
-
+  get '/current_user', to: "sessions#get_current_user"
   delete '/logout', to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
