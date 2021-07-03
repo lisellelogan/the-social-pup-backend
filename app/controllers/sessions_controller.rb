3ec869ddf56cf1d8552a7448d799408536e17afb
lisellelogan <liselle.logan@yahoo.com>
2021-07-03 14:08:46 -0400

    remove get current user action and route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,9 @@ class SessionsController < ApplicationController
     def create
         @current_user = User.find_by(email: params[:email])
         if @current_user
-            render json: @current_user
+            render json: UserSerializer.new(@current_user)
         else
             render json: {error: "We can't find you. Please try agian."}
-        end
-    end
-
-    def get_current_user
-        if !!@current_user
-            render json: UserSerializer.new(@current_user)
-        else  
-            render json: {alert: "Not logged in."}
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :friendships
   resources :users
 
-  get '/current_user', to: "sessions#get_current_user"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
