5013d851ac6a089bf428f9eccb572d9d93e55e9d
lisellelogan <liselle.logan@yahoo.com>
2021-07-05 12:35:37 -0400

    remove friendship relationship

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  has_many :friendships
   
-  def friends 
-    object.friends
-  end
-
-  attributes :id,:email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :picture, :owner_name, :friends
+  attributes :id,:email, :pup_name, :pup_age, :pup_breed, :pup_fully_vaccinated, :pup_location, :pup_personality, :picture, :owner_name
 end
