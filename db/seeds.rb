37984a6890d581a23d18a6cf24030c4e035cf164
lisellelogan <liselle.logan@yahoo.com>
2021-07-05 12:50:00 -0400

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-Friendship.destroy_all
+# Friendship.destroy_all
 
-beemo = User.create(email: 'beemo@beemo.com', password: 'beemo', pup_name: 'beemo', pup_age: 9, pup_breed: 'corgi', pup_fully_vaccinated: true, pup_location: 'queens, ny', pup_personality: 'crazy', owner_name: 'lis', picture: 'https://i.redd.it/5f0snjdbzr121.jpg')
-seymour = User.create(email: 'seymour@seymour.com', password: 'seymour', pup_name: 'seymour', pup_age: 5, pup_breed: 'beagle', pup_fully_vaccinated: true, pup_location: 'virginia beach, va', pup_personality: 'calm', owner_name: 'london', picture: 'https://preview.redd.it/mf47o59jlsd61.jpg?width=4000&format=pjpg&auto=webp&s=0dd1a3e2ed61d6ca48e934da16c193d8178814b3')
-charlie = User.create(email: 'charlie@charlie.com', password: 'charlie', pup_name: 'charlie', pup_age: 1, pup_breed: 'goldendoodle', pup_fully_vaccinated: true, pup_location: 'chesapeake, va', pup_personality: 'anxious', owner_name: 'justin', picture: 'https://i.imgur.com/9i34Gtn.jpg')
+beemo = User.create(email: 'beemo@beemo.com', password: 'beemo', pup_name: 'beemo', pup_age: "10 months old", pup_breed: 'corgi', pup_fully_vaccinated: true, pup_location: 'queens, ny', pup_personality: 'crazy', owner_name: 'lis', picture: 'https://i.redd.it/5f0snjdbzr121.jpg')
+seymour = User.create(email: 'seymour@seymour.com', password: 'seymour', pup_name: 'seymour', pup_age: "5 years old", pup_breed: 'beagle', pup_fully_vaccinated: true, pup_location: 'virginia beach, va', pup_personality: 'calm', owner_name: 'london', picture: 'https://preview.redd.it/mf47o59jlsd61.jpg?width=4000&format=pjpg&auto=webp&s=0dd1a3e2ed61d6ca48e934da16c193d8178814b3')
+charlie = User.create(email: 'charlie@charlie.com', password: 'charlie', pup_name: 'charlie', pup_age: "1 year old", pup_breed: 'goldendoodle', pup_fully_vaccinated: true, pup_location: 'chesapeake, va', pup_personality: 'anxious', owner_name: 'justin', picture: 'https://i.imgur.com/9i34Gtn.jpg')
 
-Friendship.create_mutual_friendship(beemo.id, seymour.id)
-Friendship.create_mutual_friendship(beemo.id, charlie.id)
-Friendship.create_mutual_friendship(seymour.id, charlie.id)
+# Friendship.create_mutual_friendship(beemo.id, seymour.id)
+# Friendship.create_mutual_friendship(beemo.id, charlie.id)
+# Friendship.create_mutual_friendship(seymour.id, charlie.id)
