1f7a22c6f76a554166cd4702beea7ca831d5848e
lisellelogan <liselle.logan@yahoo.com>
2021-07-05 12:50:50 -0400

    remove relationship in serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: UserSerializer.new(users, include: [:friendships])
+        render json: UserSerializer.new(users)
     end
 
     def create 
