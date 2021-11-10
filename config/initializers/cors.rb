a24057b8aec0e395f24e3f494de9a7ae27acd718
lisellelogan <liselle.logan@yahoo.com>
2021-11-10 12:55:40 -0500

    change origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://thesocialpup.netlify.app/'
+    origins '*'
 
     resource '*',
       headers: :any,
